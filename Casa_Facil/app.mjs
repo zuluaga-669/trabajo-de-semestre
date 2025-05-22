50e26bc4b72d8791098de7b070436f105d258475
ElDaniProgramator <danielvg.contacto@gmail.com>
2025-05-22 15:33:35 -0500

    Awo

--- Casa_Facil/app.mjs
+++ Casa_Facil/app.mjs
@@ -8,10 +8,15 @@ const app = express();
 app.use(express.json());
 app.use(express.static('cliente'));  // Sirve archivos estáticos como tu frontend
 
+// Ruta para la página inicial
+app.get('/', (req, res) => {
+    res.sendFile('html/home.html', { root: './cliente' });
+});
+
 // Rutas
 //app.use('/', gastoRoutes);  // Usamos las rutas definidas para gastos
 
 // Iniciar el servidor
 app.listen(port, () => {
-    console.log(`Servidor escuchando en el puerto ${port}`);
+    console.log(`Servidor escuchando en el puerto ${port}`);
 });
